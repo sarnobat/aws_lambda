3981e915a289a7bd0408634f5c946882ecab6c53
Sridhar Sarnobat <ssarnobat@control4.com>
2018-01-15 00:36:52 -0800

    Minor changes (untested)

---
+++ aws-lambda-hello-world/run.sh
@@ -0,0 +1,4 @@
+curl -v -d '{}' 'https://u80lf760ri.execute-api.us-east-1.amazonaws.com/prod/helloworldjar'
+
+# logs here:
+# https://us-east-1.console.aws.amazon.com/cloudwatch/home?region=us-east-1#logEventViewer:group=/aws/lambda/helloworldjar;stream=2018/01/15/%5B$LATEST%5D5aaee9a723ef445eaa24849a123d527c
