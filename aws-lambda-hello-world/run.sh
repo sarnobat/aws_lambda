87a78792ef3d939459c5fabdb76d6ef237110909
Sridhar Sarnobat <ss401533@gmail.com>
2018-01-18 18:54:17 -0800

    Minor changes (untested)

--- aws-lambda-hello-world/run.sh
+++ aws-lambda-hello-world/run.sh
@@ -2,3 +2,6 @@ curl -v -d '{}' 'https://u80lf760ri.execute-api.us-east-1.amazonaws.com/prod/hel
 
 # logs here:
 # https://us-east-1.console.aws.amazon.com/cloudwatch/home?region=us-east-1#logEventViewer:group=/aws/lambda/helloworldjar;stream=2018/01/15/%5B$LATEST%5D5aaee9a723ef445eaa24849a123d527c
+
+# upload jar
+aws lambda  update-function-code --function-name helloworldjar --zip-file fileb://target/aws-lambda-hello-world-1.0-SNAPSHOT.jar
